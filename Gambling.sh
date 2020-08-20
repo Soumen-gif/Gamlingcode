e1dbd1ee9067d99d4dd6ec37833796dba42b630e
soumen-gif <soumentaki1996@gmail.com>
2020-08-20 20:16:42 +0530

    either he win or loss

--- Gambling.sh
+++ Gambling.sh
@@ -1,3 +1,12 @@
 echo "Welcome to gamling code"
 stakes=100
 bet=0
+x=$((RANDOM%2))
+   if [ $x -eq 1 ]
+      then
+           echo "he won the game"
+   else
+           echo "he lost the game"
+
+      fi
+
