f6e500e224105363f5570bd4a03d3b6c09c60c84
soumen-gif <soumentaki1996@gmail.com>
2020-08-20 21:08:12 +0530

    calculated till the date

--- Gambling.sh
+++ Gambling.sh
@@ -1,12 +1,29 @@
+
 echo "Welcome to gamling code"
-stakes=100
-bet=0
-x=$((RANDOM%2))
-   if [ $x -eq 1 ]
-      then
-           echo "he won the game"
-   else
-           echo "he lost the game"
+#!/bin/bash -x
 
-      fi
+day=0
+Stakes=100
+bet=1
+winTarget=150
+loseTarget=50
+    doingBet(){
+        if [ $((RANDOM % 2)) -eq 1 ]
+        then
+                echo "he Won the match"
+                stakes=$(($stakes + $bet))
+        else
+                echo "he lose the game"
+                stakes=$(($stakes - $bet))
+        fi
+        }
 
+noOfDay(){
+        stakes=100
+        while [ $stakes -lt $winTarget ] && [ $stakes -gt $loseTraget ]
+        do
+                doingBet
+        done
+        echo "Your final stake = $stakes"
+}
+noOfDay
